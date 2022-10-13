41821f379a774dfa69a9684f7acb0f4077994f26
Utham Bathoju <uthamkanth@gmail.com>
2022-10-13 13:04:57 +0530

    Day 44

---
+++ Utham Bathoju/D44_trget_sum.py
@@ -0,0 +1,9 @@
+def findTargetSumWays(A, S):
+    count = collections.Counter({0: 1})
+    for x in A:
+        step = collections.Counter()
+        for y in count:
+            step[y + x] += count[y]
+            step[y - x] += count[y]
+        count = step
+    return count[S]
